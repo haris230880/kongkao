36ea0e86d8cca766d1352de9f287331b81f222d7
Haris <67259324+haris230880@users.noreply.github.com>
2023-05-16 17:22:59 +0700

    edit timer

--- lib/winged/seacrch.dart
+++ lib/winged/seacrch.dart
@@ -1,75 +1,3 @@
-// import 'dart:convert';
-//
-// import 'package:dio/dio.dart';
-// import 'package:flutter/material.dart';
-// import 'package:project/model/usermodel.dart';
-//
-// import '../configs/services/api.dart';
-//
-//
-// class MySearchDelegate extends SearchDelegate {
-//
-//
-//
-//
-//
-//
-//   List<String> searcResult = ['fgdrfg','sdasd'];
-//   @override
-//   Widget? buildLeading(BuildContext context) => IconButton(
-//       onPressed: () => close(context, null), icon: Icon(Icons.arrow_back));
-//
-//   @override
-//   List<Widget>? buildActions(BuildContext context) => [
-//         IconButton(
-//             onPressed: () {
-//               if (query.isEmpty) {
-//                 close(context, null);
-//               } else {
-//                 query = '';
-//               }
-//             },
-//             icon: Icon(Icons.clear))
-//       ];
-//
-//   @override
-//   Widget buildResults(BuildContext context) => Center(
-//         child: Text(query),
-//       );
-//
-//   @override
-//   Widget buildSuggestions(BuildContext context) {
-//     List<String> suggestions = searcResult.where((searcResult) {
-//       final result = searcResult.toLowerCase();
-//       final input = query.toLowerCase();
-//
-//       return result.contains(input);
-//     }).toList();
-//     return ListView.builder(
-//       itemCount: suggestions.length,
-//       itemBuilder: (context, index) {
-//         final suggestio = suggestions[index];
-//
-//         return ListTile(
-//           title: Text(suggestio),
-//           onTap: () {
-//             query = suggestio;
-//
-//             showResults(context);
-//           },
-//         );
-//       },
-//     );
-//   }
-// }
-//
-//
-//
-//
-//
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
 import 'dart:async';
 import 'dart:convert';
 
@@ -276,7 +204,7 @@ class MySearchDelegate extends SearchDelegate {
     _debounceTimer?.cancel();
 
     // Set a new timer to delay the API request
-    _debounceTimer = Timer(Duration(milliseconds: 2000), () {
+    _debounceTimer = Timer(Duration(milliseconds: 1000), () {
       showResults(context);
     });
 
