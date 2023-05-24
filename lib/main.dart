0fff759790186160543665fa0da6b44e32a2bd25
Haris <67259324+haris230880@users.noreply.github.com>
2023-05-25 03:54:51 +0700

    otp

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:project/constants.dart';
@@ -6,7 +7,9 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:toast/toast.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
--- lib/screen/Regis/components/otp/otp_regisSale.dart
+++ lib/screen/Regis/components/otp/otp_regisSale.dart
@@ -1,180 +1,12 @@
-// import 'package:email_auth/email_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:project/configs/services/datauserbay.dart';
-// import 'package:project/screen/Login/components/login_screen.dart';
-// import 'package:project/screen/Regis/components/background_regis.dart';
-// import 'package:project/screen/Regis/components/regis.dart';
-//
-// import '../../../constants.dart';
-//
-//
-//
-//
-// class Otp_Regis extends StatefulWidget {
-//   const Otp_Regis({Key? key}) : super(key: key);
-//
-//   @override
-//   State<Otp_Regis> createState() => _Otp_RegisState();
-// }
-//
-// class _Otp_RegisState extends State<Otp_Regis> {
-//   @override
-//   Widget build(BuildContext context) {
-//     Size size = MediaQuery.of(context).size;
-//     return BackgroundRegis(
-//       child: SingleChildScrollView(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               "Kongkao",
-//               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-//             ),
-//             SizedBox(
-//               height: 10,
-//             ),
-//             Image.asset(
-//               'assets/images/logo.jpg',
-//               scale: 10,
-//             ),
-//             SizedBox(
-//               height: 20,
-//             ),
-//             Container(
-//               margin: EdgeInsets.all(20),
-//               padding:
-//               EdgeInsets.only(left: 50, right: 50, top: 10, bottom: 20),
-//               alignment: Alignment.center,
-//               decoration: BoxDecoration(
-//                   color: Colors.white,
-//                   border: Border.all(color: kPrimaryColor),
-//                   borderRadius: BorderRadius.all(Radius.circular(20.0)),
-//                   boxShadow: [
-//                     BoxShadow(
-//                         blurRadius: 2,
-//                         color: kPrimaryColor,
-//                         offset: Offset(1, 1))
-//                   ]),
-//               child: Column(
-//                 children: <Widget>[
-//                   Text('OTP'),
-//                   SizedBox(
-//                     height: 10,
-//                   ),
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                     children: [
-//                     textFildotp(first: true, last: false),
-//                     textFildotp(first: false, last: false),
-//                     textFildotp(first: false, last: false),
-//                     textFildotp(first: false, last: true),
-//                   ],),
-//
-//
-//                   SizedBox(
-//                     height: 10,
-//                   ),
-//                   Text('กรุณากรอกรหัส OTP'),
-//                 ],
-//               ),
-//             ),
-//             TextButton(
-//               onPressed: () {},
-//               child: Text('ส่งรหัสอีกครั้ง'),
-//             ),
-//             Text('กรุณารอส่งรหัสอีกครั้งใน 2:33 นาที'),
-//             SizedBox(
-//               height: 10,
-//             ),
-//             ElevatedButton(
-//               style: ElevatedButton.styleFrom(
-//                   shape: RoundedRectangleBorder(
-//                     borderRadius: BorderRadius.circular(20),
-//                   ),
-//                   elevation: 5,
-//                   // Foreground color
-//                   onPrimary: Colors.white,
-//                   // Background color
-//                   primary: kPrimaryColor,
-//                   minimumSize: Size(30, 50))
-//                   .copyWith(elevation: ButtonStyleButton.allOrNull(5.0)),
-//               onPressed: () {
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(
-//                     builder: (context) {
-//                       return LoginScreen();
-//                     },
-//                   ),
-//                 );
-//               },
-//               child: const Text('ยืนยัน otp'),
-//             ),
-//             TextButton(
-//               onPressed: () {
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(
-//                     builder: (context) {
-//                       return RegisScreen();
-//                     },
-//                   ),
-//                 );
-//               },
-//               child: const Text(
-//                 'ยกเลิก',
-//                 style: TextStyle(color: kPrimaryLightColor),
-//               ),
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//   textFildotp({required bool first, last}) {
-//     return Container(
-//         height: 85,
-//         child: AspectRatio(
-//           aspectRatio: 0.7,
-//           child: TextField(
-//             autofocus: true,
-//             onChanged: (value) {
-//               if(value.length==1&&last==false){
-//                 FocusScope.of(context).nextFocus();
-//               }  if(value.length==0&&first==false){
-//                 FocusScope.of(context).previousFocus();
-//               }
-//
-//             },
-//             showCursor: false,
-//             readOnly: false,
-//             textAlign: TextAlign.center,
-//             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-//             keyboardType: TextInputType.number,
-//             maxLength: 1,
-//             decoration: InputDecoration(
-//               counter: Offstage(),
-//               enabledBorder: OutlineInputBorder(
-//                 borderRadius: BorderRadius.circular(12),
-//                 borderSide: BorderSide(width: 2, color: Colors.black26),
-//               ),
-//               focusedBorder: OutlineInputBorder(
-//                 borderRadius: BorderRadius.circular(12),
-//                 borderSide: BorderSide(width: 2, color: kPrimaryColor),
-//               ),
-//             ),
-//           ),
-//         ));
-//   }
-//   }
-//
 
 
 import 'dart:math';
 
 import 'package:dio/dio.dart';
 import 'package:email_auth/email_auth.dart';
-import 'package:email_validator/email_validator.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:project/configs/services/datauserbay.dart';
 import 'package:project/screen/Login/components/login_screen.dart';
@@ -195,60 +27,63 @@ class Otp_RegisSale extends StatefulWidget {
 }
 
 class _Otp_RegisSaleState extends State<Otp_RegisSale> {
-  /// The boolean to handle the dynamic operations
+
   bool submitValid = false;
 
-  /// Text editing controllers to get the value from text fields
+
   final TextEditingController _emailcontroller = TextEditingController();
   final TextEditingController _otpcontroller = TextEditingController();
-
-  // Declare the object
-  late EmailAuth emailAuth;
+  String? _verificationId;
 
   @override
   void initState() {
     super.initState();
-    // Initialize the package
-    emailAuth = new EmailAuth(
-      sessionName: "Sample session",
-    );
-
-    /// Configuring the remote server
-     emailAuth.config({});
+    initializeFirebase();
   }
 
-  /// a void function to verify if the Data provided is true
-  /// Convert it into a boolean function to match your needs.
-  void verify() {
-
-    var res = emailAuth.validateOtp(recipientMail: _emailcontroller.text, userOtp: _otpcontroller.text);
-
-    if(res){
-      print('OTP Verify');
-      uplodeimageusersaveusersale();
-    }else{
-      normaDiolog(context, "OTP ไม่ถูกต้อง");
-      print('Invalid OTP ');
-    }
-
-
-    print(emailAuth.validateOtp(
-        recipientMail: _emailcontroller.value.text,
-        userOtp: _otpcontroller.value.text));
-  
+  void initializeFirebase() {
+    Firebase.initializeApp();
   }
 
-  /// a void funtion to send the OTP to the user
-  /// Can also be converted into a Boolean function and render accordingly for providers
   void sendOtp() async {
-    bool result = await emailAuth.sendOtp(
-        recipientMail: _emailcontroller.value.text, otpLength:5);
-    if (result) {
-      setState(() {
-        submitValid = true;
-      });
+    try {
+      await FirebaseAuth.instance.verifyPhoneNumber(
+        phoneNumber: _emailcontroller.value.text,
+        verificationCompleted: (PhoneAuthCredential credential) {},
+        verificationFailed: (FirebaseAuthException e) {
+          print(e.message);
+        },
+        codeSent: (String verificationId, int? resendToken) {
+          setState(() {
+            submitValid = true;
+            _verificationId = verificationId;
+          });
+        },
+        codeAutoRetrievalTimeout: (String verificationId) {},
+      );
+    } catch (e) {
+      print(e.toString());
     }
   }
+  void verify() async {
+    try {
+      PhoneAuthCredential credential = PhoneAuthProvider.credential(verificationId: _verificationId!, smsCode: _otpcontroller.value.text);
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+      User? user = userCredential.user;
+
+      if (user != null) {
+        print('OTP Verified');
+        uplodeimageusersaveusersale();
+      } else {
+        print('Invalid OTP');
+        normaDiolog(context, "OTP ไม่ถูกต้อง");
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -299,9 +134,6 @@ class _Otp_RegisSaleState extends State<Otp_RegisSale> {
                     height: 50,
                     width: 340,
                     child: TextFormField(
-                      validator: (selluser_email) => EmailValidator.validate(selluser_email!)
-                          ? null
-                          : "กรอกอีเมล ลงท้ายด้วย@gmail.com",
                       onChanged: (value) => selluser_email  = value.trim(),
                       controller: _emailcontroller,
                       keyboardType: TextInputType.text,
@@ -423,24 +255,7 @@ class _Otp_RegisSaleState extends State<Otp_RegisSale> {
                         )
                       : SizedBox(height: 1)
 
-                  //
-                  // SizedBox(
-                  //   height: 10,
-                  // ),
-                  // Row(
-                  //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  //   children: [
-                  //     textFildotp(first: true, last: false),
-                  //     textFildotp(first: false, last: false),
-                  //     textFildotp(first: false, last: false),
-                  //     textFildotp(first: false, last: true),
-                  //   ],),
-                  //
-                  //
-                  // SizedBox(
-                  //   height: 10,
-                  // ),
-                  // Text('กรุณากรอกรหัส OTP'),
+
                 ],
               ),
             ),
